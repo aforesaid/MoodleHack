29788c7ba441eb0dee67c86ad437527db07ba6e0
y.fomenkov <mail.ru>
2021-11-12 13:34:18 +0300

    Add controller for validate accounts

--- MoodleHack.API/Controllers/AdminController.cs
+++ MoodleHack.API/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using MoodleHack.API.Services;
 using MoodleHack.Infrastructure.Data;
 
 namespace MoodleHack.API.Controllers
@@ -12,11 +13,13 @@ namespace MoodleHack.API.Controllers
     {
         private readonly AppDbContext _dbContext;
         private readonly IConfiguration _configuration;
+        private readonly SdoApiClient _sdoApiClient;
 
-        public AdminController(AppDbContext dbContext, IConfiguration configuration)
+        public AdminController(AppDbContext dbContext, IConfiguration configuration, SdoApiClient sdoApiClient)
         {
             _dbContext = dbContext;
             _configuration = configuration;
+            _sdoApiClient = sdoApiClient;
         }
 
         public async Task<IActionResult> GetAllAccounts(string token)
@@ -24,6 +27,26 @@ namespace MoodleHack.API.Controllers
             if (_configuration["TOKEN"] == token)
                 return Ok(await _dbContext.Users.AsQueryable().Where(x => x.IsActive)
                     .ToArrayAsync());
+            return Forbid();
+        }
+
+        public async Task<IActionResult> SyncAccounts(string token)
+        {
+            if (_configuration["TOKEN"] == token)
+            {
+                foreach (var account in _dbContext.Users.AsQueryable().Where(x => x.IsActive))
+                {
+                    var valid = await _sdoApiClient.Validate(account.Cookie);
+                    if (!valid)
+                    {
+                        account.IsActive = false;
+                        _dbContext.Update(account);
+                    }
+                }
+                await _dbContext.SaveChangesAsync();
+                return Ok(_dbContext.Users.Where(x => x.IsActive).ToArray());
+            }
+
             return Forbid();
         }
         //Remove all
